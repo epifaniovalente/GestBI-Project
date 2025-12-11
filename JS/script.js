d794e24bb8b64efa1ce9f0dababda5bd18231ebf
EpiCoder_# <gestbiregistocivil@gmail.com>
2025-12-11 06:50:59 +0100

    Second meeting
    HomePage, Sobre

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+const services_wrapper = document.getElementById("services-wrapper");
+const services_details = document.getElementById("services-details");
+
+services_wrapper.addEventListener("click", () => {
+    services_details.classList.toggle('open')
+})
+
+const menu_btn = document.getElementById("menu-toggle");
+const nav_menu = document.getElementById("nav-menu");
+
+menu_btn.addEventListener('click', () => {
+    nav_menu.classList.toggle('open')
+})
+
+
+// Fechar o menu quando clicar fora.
+document.addEventListener('click', (e) => {
+    const target = e.target;
+    if (!nav_menu.classList.contains('open')) return;
+    if (nav_menu.contains(target) || menu_btn.contains(target)) return;
+    nav_menu.classList.remove('open')
+})
+
+
+// Fechar o menu quando clicar no Esc (Escape)
+
+document.addEventListener('keydown', (letra) => {
+    if (letra.key === 'Escape') {
+        if (nav_menu.classList.contains('open')) {
+            nav_menu.classList.remove('open');
+        }
+
+    }
+})
